b6ae6c26d7f470bde3aee93ac1798fea0334662f
backdfb <wva22lfr@gmail.com>
2025-02-12 16:58:11 +0900

    모달창 추가 및 스타일 작성성

--- vite-project/src/App.svelte
+++ vite-project/src/App.svelte
@@ -1,9 +1,45 @@
 <script>
   const data = [
-    { title: '쉬리', openDate: 1998, category: '액션', who:'한석규, 최민식', director:'강제규', likeCount: 0, imgUrl: 'images/쉬리.jpg'},
-    { title: '물위의 하룻밤', openDate: 1998, category: '드라마', who:'이승희', director:'강정수', likeCount: 0, imgUrl: 'images/물위의하룻밤.jpg'},
-    { title: '파묘', openDate: 2024, category: '호러', who:'최민식, 유해진, 김고은, 이도현', director:'장재현', likeCount: 0, imgUrl: 'images/파묘.jpg'},
-    { title: '하얼빈', openDate: 2025, category: '드라마', who:'현빈', director:'우민호', likeCount: 0, imgUrl: 'images/하얼빈.jpg'},
+    { 
+      title: '쉬리', 
+      openDate: 1998, 
+      category: '액션', 
+      who:'한석규, 최민식', 
+      director:'강제규', 
+      likeCount: 0, 
+      imgUrl: 'images/쉬리.jpg',
+      story: '토종 물고기 다큐멘터리',
+    },
+    { 
+      title: '물위의 하룻밤', 
+      openDate: 1998, 
+      category: '드라마', 
+      who:'이승희', 
+      director:'강정수', 
+      likeCount: 0, 
+      imgUrl: 'images/물위의하룻밤.jpg',
+      story: '수상도시를 건설하는 휴먼 드라마'
+    },
+    { 
+      title: '파묘', 
+      openDate: 2024, 
+      category: '호러', 
+      who:'최민식, 유해진, 김고은, 이도현', 
+      director:'장재현', 
+      likeCount: 0, 
+      imgUrl: 'images/파묘.jpg',
+      story: '전설의 무기를 찾아 모험을 떠나는 4명의 용사 이야기'
+    },
+    { 
+      title: '하얼빈', 
+      openDate: 2025, 
+      category: '드라마', 
+      who:'현빈', 
+      director:'우민호', 
+      likeCount: 0, 
+      imgUrl: 'images/하얼빈.jpg',
+      story: '차마 드립 칠 수 없는 역사적 이야기'
+    },
   ] // 더미 데이터 목록
 
     let likeCount = 0; // 좋아요 수 저장 변수
@@ -11,6 +47,8 @@
     const handleLike = (i) => {
       data[i].likeCount += 1;
     }
+
+    let isModal = false; // 모달 상태 저장 변수 추가가
 </script>
 
 <main class="container">
@@ -35,8 +73,14 @@
     </div>
   </div> 
   {/each}
-
 </main>
+
+<div class="modal">
+  <div class="inner">
+    <h3>자세히</h3>
+    <button class="btn-close">닫기</button>
+  </div>
+</div>
 
 <style>
   .bg-yellow {
@@ -62,5 +106,24 @@
 
   .item .info {
     width: 100%;
+  }
+
+  .modal {
+    background: rgba(0, 0, 0, 0.7);
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100vh;
+    display: flex;  
+    justify-content: center;
+    align-items: center; 
+  } 
+
+  .modal .inner {
+    background: #fff;
+    width: 80%;
+    padding: 20px;
+    border-radius: 10px;
   }
 </style>
