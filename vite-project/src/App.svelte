ab0430a9feb1f15514a81d67775fc02e868587a9
backdfb <wva22lfr@gmail.com>
2025-02-12 15:18:47 +0900

    스타일(글자색) 변경

--- vite-project/src/App.svelte
+++ vite-project/src/App.svelte
@@ -1,28 +1,25 @@
 <script>
-  import logo1 from './assets/svelte.svg';
-  let logo2 = '/vite.svg';
-
-  let textRed = 'color: red';
-
   const data = [
     { title: '물위의 하룻밤', openDate: 1998, category: '드라마' },
     { title: '파묘', openDate: 2024, category: '호러' },
     { title: '차인표', openDate: 2020, category: '코미디' },
   ] 
+
+    // 글자색 스타일 변수 let textRed = 'color: red'; 
+    let likeCount = 0; // 좋아요 수 저장 변수
 </script>
 
 <main>
 
-  <h1>영화정보</h1>
-  <img src={logo1} alt="">
-  <img src={logo2} alt="">
-  {#each data as movie, i}
+  <h1>개밥그릇의 인생영화 목록</h1>
+  {#each data as movie, i} 
   <div>
-    <h3 class="bg-yellow" style={textRed}>{movie.title}</h3>
+    <h3 class="bg-yellow">{movie.title}</h3>
     <h3>{movie.title}</h3>
     <p>개봉: {movie.openDate}</p>
     <p>장르: {movie.category}</p>
-  </div>
+    <button>좋아요 {likeCount}</button>
+  </div> 
   {/each}
 
 </main>
@@ -31,5 +28,6 @@
   .bg-yellow {
     background-color: gold;
     padding: 10px;
+    color: #333;
   }
 </style>
