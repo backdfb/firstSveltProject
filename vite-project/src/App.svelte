82b431f6a5738300366ee0d162abee970b0f3dd8
backdfb <wva22lfr@gmail.com>
2025-02-12 17:37:10 +0900

    영화 데이터 배열을 별도 파일로 분리하여 관리

--- vite-project/src/App.svelte
+++ vite-project/src/App.svelte
@@ -1,47 +1,6 @@
 <script>
-  const data = [
-    { 
-      title: '쉬리', 
-      openDate: 1998, 
-      category: '액션', 
-      who:'한석규, 최민식', 
-      director:'강제규', 
-      likeCount: 0, 
-      imgUrl: 'images/쉬리.jpg',
-      story: '토종 물고기 다큐멘터리',
-    },
-    { 
-      title: '물위의 하룻밤', 
-      openDate: 1998, 
-      category: '드라마', 
-      who:'이승희', 
-      director:'강정수', 
-      likeCount: 0, 
-      imgUrl: 'images/물위의하룻밤.jpg',
-      story: '수상도시를 건설하는 휴먼 드라마'
-    },
-    { 
-      title: '파묘', 
-      openDate: 2024, 
-      category: '호러', 
-      who:'최민식, 유해진, 김고은, 이도현', 
-      director:'장재현', 
-      likeCount: 0, 
-      imgUrl: 'images/파묘.jpg',
-      story: '전설의 무기를 찾아 모험을 떠나는 4명의 용사 이야기'
-    },
-    { 
-      title: '하얼빈', 
-      openDate: 2025, 
-      category: '드라마', 
-      who:'현빈', 
-      director:'우민호', 
-      likeCount: 0, 
-      imgUrl: 'images/하얼빈.jpg',
-      story: '차마 드립 칠 수 없는 역사적 이야기'
-    },
-  ] // 더미 데이터 목록
-
+  import {data} from './lib/movies'; 
+  // data 배열은 영화 정보를 담고 있음
     let likeCount = 0; // 좋아요 수 저장 변수
 
     const handleLike = (i) => {
---
+++ vite-project/src/lib/movies.js
@@ -0,0 +1,44 @@
+const data = [
+    { 
+      title: '쉬리', 
+      openDate: 1998, 
+      category: '액션', 
+      who:'한석규, 최민식', 
+      director:'강제규', 
+      likeCount: 0, 
+      imgUrl: 'images/쉬리.jpg',
+      story: '토종 물고기 다큐멘터리',
+    },
+    { 
+      title: '물위의 하룻밤', 
+      openDate: 1998, 
+      category: '드라마', 
+      who:'이승희', 
+      director:'강정수', 
+      likeCount: 0, 
+      imgUrl: 'images/물위의하룻밤.jpg',
+      story: '수상도시를 건설하는 휴먼 드라마'
+    },
+    { 
+      title: '파묘', 
+      openDate: 2024, 
+      category: '호러', 
+      who:'최민식, 유해진, 김고은, 이도현', 
+      director:'장재현', 
+      likeCount: 0, 
+      imgUrl: 'images/파묘.jpg',
+      story: '전설의 무기를 찾아 모험을 떠나는 4명의 용사 이야기'
+    },
+    { 
+      title: '하얼빈', 
+      openDate: 2025, 
+      category: '드라마', 
+      who:'현빈', 
+      director:'우민호', 
+      likeCount: 0, 
+      imgUrl: 'images/하얼빈.jpg',
+      story: '차마 드립 칠 수 없는 역사적 이야기'
+    },
+  ] // 더미 데이터 목록
+
+  export {data};
