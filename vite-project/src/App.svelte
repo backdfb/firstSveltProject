1a31f4b4f2002b10c5f443d7b5b8d02b0b456563
backdfb <wva22lfr@gmail.com>
2025-02-14 14:10:25 +0900

    영화 목록 컴포넌트 추가 및 이벤트 창 구현

--- vite-project/src/App.svelte
+++ vite-project/src/App.svelte
@@ -2,6 +2,8 @@
   import {data} from './lib/movies'; // data 배열은 영화 정보를 담고 있음
   import Navbar from './lib/components/Navbar.svelte';
   import Modal from './lib/components/Modal.svelte'; // Modal 컴포넌트 추가
+  import Movies from './lib/components/Movies.svelte'; // 영화 데이터 가져오기
+
   let likeCount = 0; // 좋아요 수 저장 변수
   const handleLike = (i) => {
     data[i].likeCount += 1;
@@ -13,36 +15,19 @@
   const closeModal = () => {
     isModal = false;
   }
+
+  const handleMovieNumber = (i) => {
+    selectedMovie = i;
+  } 
+
+  let isEvent = true; // 이벤트 창 표시 여부
 </script>
 
-<Navbar />
-<main class="container">
-  <h1>개밥그릇의 인생영화 목록</h1>
-  {#each data as movie, i} 
-  <!-- 반복문, data 배열을 순회하며 movie 객체와 인덱스 i를 반환 -->
-  <div class="item">
-    <figure>
-      <img src={movie.imgUrl} alt={movie.title} />
-    </figure>
-    <div class="info">
-      <h3 class="bg-yellow">{movie.title}</h3>
-      <h3>{movie.title}</h3>
-      <p>개봉: {movie.openDate}</p>
-      <p>장르: {movie.category}</p>
-      <p>주연: {movie.who}</p>
-      <p>감독: {movie.director}</p>
-      <button 
-        on:click={() => {handleLike(i)}}>
-        <!-- on:click은 클릭 이벤트 처리하는 svelte 문법으로, 좋아요 버튼 클릭 시 handleLike 함수 호출 -->
-        좋아요 {data[i].likeCount}</button>
-        <button on:click={() => {
-          isModal = true;
-          selectedMovie =i;
-        }} class="btn btn-primary">상세보기</button>
-    </div>
-  </div> 
-  {/each}
-</main>
+<div class={isEvent ? 'event show' : 'event'}>
+  <p>DFBFLEX 운명의 드라마, 경기크리처</p>
+  <button>X</button>
+</div>
+<Movies {data} bind:isModal {handleMovieNumber} {handleLike} />
 
 {#if isModal}
   <Modal 
@@ -54,28 +39,39 @@
 {/if}
 
 <style>
-  .bg-yellow {
-    background-color: gold;
-    padding: 10px;
-    color: #333;
-  }
-  .item {
+  .event {
+    width: 100%;
+    background: #666;
+    padding: 5px 1em;
     display: flex;
-    width: 100%;
-    margin-bottom: 20px;
-    padding: 1rem;
-    border: 1px solid #ccc;
-  }
-  .item figure {
-    width: 30%;
-    margin-right: 1rem;
+    justify-content: space-between;
+    align-items: center;
+    color: #fff;
+    text-align: center;
+    margin-bottom: 1em;
+    padding: 10px 20px;
+
+    /* 창이 보이지 않게 */
+    max-height: 0;
+    opacity: 0;
+    overflow: hidden;
+    transition: all 0.4s;
   }
 
-  .item img {
-    width: 100%;
+  /* 기본: 창이 보이게 */
+  .show {
+    opacity: 1;
+    max-height: 100px;
   }
 
-  .item .info {
+  .event button {
+    padding: 2px;
+  }
+
+  .event p, .event button {
+    margin: 0;
+  }
+  .event p {
     width: 100%;
   }
 </style>
---
+++ vite-project/src/lib/components/Movies.svelte
@@ -0,0 +1,63 @@
+<script>
+    export let data=[];
+    export let isModal = false;
+    export let handleLike;
+    export let handleMovieNumber; 
+</script>
+
+<main class="container">
+    <h1>개밥그릇의 인생영화 목록</h1>
+    {#each data as movie, i} 
+    <!-- 반복문, data 배열을 순회하며 movie 객체와 인덱스 i를 반환 -->
+    <div class="item">
+      <figure>
+        <img src={movie.imgUrl} alt={movie.title} />
+      </figure>
+      <div class="info">
+        <h3 class="bg-yellow">{movie.title}</h3>
+        <h3>{movie.title}</h3>
+        <p>개봉: {movie.openDate}</p>
+        <p>장르: {movie.category}</p>
+        <p>주연: {movie.who}</p>
+        <p>감독: {movie.director}</p>
+        <button 
+          on:click={() => {handleLike(i)}}>
+          좋아요 {data[i].likeCount}</button>
+          <!-- on:click은 클릭 이벤트 처리하는 svelte 문법으로, 좋아요 버튼 클릭 시 handleLike 함수 호출 -->
+          <button on:click={() => {
+            isModal = true;
+            handleMovieNumber(i);
+          }} class="btn btn-primary">상세보기</button>
+      </div>
+    </div> 
+    {/each}
+  </main>
+
+  <style>
+    .bg-yellow {
+    background-color: gold;
+    padding: 10px;
+    color: #333;
+    }
+
+    .item {
+    display: flex;
+    width: 100%;
+    margin-bottom: 20px;
+    padding: 1rem;
+    border: 1px solid #ccc;
+    }
+
+    .item figure {
+    width: 30%;
+    margin-right: 1rem;
+    }
+
+    .item img {
+    width: 100%;
+    }
+
+    .item .info {
+    width: 100%;
+    }
+  </style>
